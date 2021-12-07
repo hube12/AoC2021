5639b3bd431560aadce23ee6cccaa2a02247d502
Neil <h66r10@gmail.com>
2021-12-07 22:14:44 +0100

    day5

--- rust/src/days/day5.rs
+++ rust/src/days/day5.rs
@@ -1,58 +1,23 @@
 use aoc_2021::{Day, Solution1, Solution2};
 
 #[derive(Default)]
-pub struct Day2;
+pub struct Day5;
 
-impl Day for Day2 {}
+impl Day for Day5 {}
 
 use anyhow::anyhow;
 
-impl Solution1 for Day2 {
+impl Solution1 for Day5 {
 
     fn run_solution1(&self, lines: Vec<String>) -> anyhow::Result<String> {
-        let res = lines.iter().map(|x| {
-            let (order, distance) = x.split_once(" ").ok_or(anyhow::Error::msg("Not splittable"))?;
-            let distance = isize::from_str_radix(distance, 10).map_err(|x| anyhow!(x))?;
-            match order {
-                "forward" => Ok((distance, 0)),
-                "down" => Ok((0, distance)),
-                "up" => Ok((0, -distance)),
-                _ => Err(anyhow::Error::msg("Not a valid order"))
-            }
-        }).fold(Ok((0isize, 0isize)), |acc: anyhow::Result<_>, x| {
-            let x = x?;
-            acc.map(|(mut horizontal, mut depth)| {
-                horizontal += x.0;
-                depth += x.1;
-                (horizontal, depth)
-            })
-        })?;
 
-        Ok((res.0 * res.1).to_string())
+        Ok(0.to_string())
     }
 }
 
-impl Solution2 for Day2 {
+impl Solution2 for Day5 {
     fn run_solution2(&self, lines: Vec<String>) -> anyhow::Result<String> {
-        let res = lines.iter().map(|x| {
-            let (order, distance) = x.split_once(" ").ok_or(anyhow::Error::msg("Not splittable"))?;
-            let distance = isize::from_str_radix(distance, 10).map_err(|x| anyhow!(x))?;
-            match order {
-                "forward" => Ok((distance, 0)),
-                "down" => Ok((0, distance)),
-                "up" => Ok((0, -distance)),
-                _ => Err(anyhow::Error::msg("Not a valid order"))
-            }
-        }).fold(Ok((0isize, 0isize, 0isize)), |acc: anyhow::Result<_>, x| {
-            let x = x?;
-            acc.map(|(mut horizontal, mut depth, mut aim)| {
-                horizontal += x.0;
-                aim += x.1;
-                depth += aim * x.0;
-                (horizontal, depth, aim)
-            })
-        })?;
-        Ok((res.0 * res.1).to_string())
+        Ok(0.to_string())
     }
 }
 
@@ -64,25 +29,25 @@ mod test {
 
     #[test]
     fn solution1() {
-        let lines: Vec<String> = collect_file(Part1, "Day2").unwrap();
-        let _ = dbg!(Day2::default().run_solution1(lines));
+        let lines: Vec<String> = collect_file(Part1, "Day5").unwrap();
+        let _ = dbg!(Day5::default().run_solution1(lines));
     }
 
     #[test]
     fn test_solution1() {
-        let lines: Vec<String> = collect_file(Test, "Day2").unwrap();
-        let _ = dbg!(Day2::default().run_solution1(lines));
+        let lines: Vec<String> = collect_file(Test, "Day5").unwrap();
+        let _ = dbg!(Day5::default().run_solution1(lines));
     }
 
     #[test]
     fn solution2() {
-        let lines: Vec<String> = collect_file(Part2, "Day2").unwrap();
-        let _ = dbg!(Day2::default().run_solution2(lines));
+        let lines: Vec<String> = collect_file(Part2, "Day5").unwrap();
+        let _ = dbg!(Day5::default().run_solution2(lines));
     }
 
     #[test]
     fn test_solution2() {
-        let lines: Vec<String> = collect_file(Test, "Day2").unwrap();
-        let _ = dbg!(Day2::default().run_solution2(lines));
+        let lines: Vec<String> = collect_file(Test, "Day5").unwrap();
+        let _ = dbg!(Day5::default().run_solution2(lines));
     }
 }
